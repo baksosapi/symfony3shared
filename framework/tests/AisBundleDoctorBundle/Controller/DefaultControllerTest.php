dd1e1bd25ec2a799147cfaea9e556ea810acc140
wildan.mbp <wildan.ubg@gmail.com>
2016-09-19 13:27:24 +0800

    new test

---
+++ framework/tests/AisBundleDoctorBundle/Controller/DefaultControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Ais\Bundle\DoctorBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DefaultControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/');
+
+        $this->assertContains('Hello World', $client->getResponse()->getContent());
+    }
+}
